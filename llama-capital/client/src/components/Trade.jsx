bc946456f1df39d4a41bd3d582863d01a1d9529c
Rolando Gomez <93492013+rolando-xyz@users.noreply.github.com>
2022-05-06 10:14:58 -0700

    llama coin slider commit

--- llama-capital/client/src/components/Trade.jsx
+++ llama-capital/client/src/components/Trade.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import CoinInfo from './CoinInfo'
-import CoinSlider from "./CoinSlider"
-import RegistrationForm from "./RegistrationForm"
 
 export default class Trade extends Component {
   
   render() {
     return (
       <div>
-        <CoinSlider />
-        <CoinSlider />
+        <CoinInfo />
+        <CoinInfo />
       </div>
     )
   }
