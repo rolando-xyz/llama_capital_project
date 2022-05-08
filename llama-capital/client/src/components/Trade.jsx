22fd66dc8e315bef2dac2b3ed76fb7e808dd9d8c
Rolando Gomez <93492013+rolando-xyz@users.noreply.github.com>
2022-05-08 08:50:36 -0700

    llama sliders commit

--- llama-capital/client/src/components/Trade.jsx
+++ llama-capital/client/src/components/Trade.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import CoinInfo from './CoinInfo'
+import CoinInfoTwo from './CoinInfoTwo'
 
 export default class Trade extends Component {
   
   render() {
     return (
       <div>
-        <CoinInfo />
         <CoinInfo />
       </div>
     )
